7e2b27c9f602e46dadce41894bf5fc36b6e6a922
jjk5158 <jjk5158@naver.com>
2025-06-16 14:42:22 +0900

    INIT : 스프링 프로젝트 설정

---
+++ src/main/java/toyproject/config/RootConfig.java
@@ -0,0 +1,58 @@
+package toyproject.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableTransactionManagement
+@ComponentScan(basePackages = "toyproject")
+@MapperScan("toyproject")
+@PropertySource("classpath:jdbc.properties")
+public class RootConfig {
+
+    @Value("${db.driverClassName}")
+    private String driverClassName;
+
+    @Value("${db.url}")
+    private String jdbcUrl;
+
+    @Value("${db.username}")
+    private String username;
+
+    @Value("${db.password}")
+    private String password;
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig config = new HikariConfig();
+        config.setDriverClassName(driverClassName);
+        config.setJdbcUrl(jdbcUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        return new HikariDataSource(config);
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean factory = new SqlSessionFactoryBean();
+        factory.setDataSource(dataSource);
+        return factory.getObject();
+    }
+
+    @Bean
+    public DataSourceTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
+    }
+}
---
+++ src/main/java/toyproject/config/WebAppInitializer.java
@@ -0,0 +1,30 @@
+package toyproject.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class WebAppInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        // 루트 설정
+        AnnotationConfigWebApplicationContext rootContext = new AnnotationConfigWebApplicationContext();
+        rootContext.register(RootConfig.class);
+        servletContext.addListener(new ContextLoaderListener(rootContext));
+
+        // 서블릿 설정
+        AnnotationConfigWebApplicationContext servletContextConfig = new AnnotationConfigWebApplicationContext();
+        servletContextConfig.register(WebConfig.class);
+
+        ServletRegistration.Dynamic dispatcher =
+            servletContext.addServlet("dispatcher", new DispatcherServlet(servletContextConfig));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
+}
