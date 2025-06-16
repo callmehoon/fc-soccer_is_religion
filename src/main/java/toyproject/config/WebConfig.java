630bea65cc0408d44b2fbc0724dcb765cf258a78
jjk5158 <jjk5158@naver.com>
2025-06-16 14:33:05 +0900

    INIT : 스프링 프로젝트 설정

---
+++ src/main/java/toyproject/config/WebConfig.java
@@ -0,0 +1,29 @@
+package toyproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "toyproject.controller")
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        // 정적 리소스(css, js 등) 경로 매핑
+        registry.addResourceHandler("/static/**")
+                .addResourceLocations("/resources/static/");
+    }
+
+    @Bean
+    public InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/WEB-INF/views/");
+        resolver.setSuffix(".jsp");
+        return resolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,11 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8" %>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>10즈 프로젝트</title>
+</head>
+<body>
+    <h2>10즈 프로젝트 구현 시작!!</h2>
+</body>
+</html>
